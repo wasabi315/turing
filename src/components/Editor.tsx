e47a46fff41db858683350958ae59b7d4b355175
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-05-28 17:09:58 +0900

    Add drawing arrow between circles

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Circle from '../shapes/Circle';
+import Arrow from '../shapes/Arrow';
 import { Point } from '../utils';
 
 interface EditorProps {
@@ -8,7 +9,9 @@ interface EditorProps {
 }
 
 interface EditorState {
+  arrows: Arrow[];
   circles: Circle[];
+  arrowStart: number | null;
   isDragging: boolean;
   dragTarget: number | null;
 }
@@ -21,7 +24,9 @@ class Editor extends React.Component<EditorProps, EditorState> {
   constructor(props: EditorProps) {
     super(props);
     this.state = {
+      arrows: [],
       circles: [],
+      arrowStart: null,
       isDragging: false,
       dragTarget: null,
     }
@@ -34,19 +39,21 @@ class Editor extends React.Component<EditorProps, EditorState> {
       this.ctx = ctx;
     }
     this.renderCanvas();
+    this.canvas.addEventListener('click', this.onClick);
+    this.canvas.addEventListener('dblclick', this.onDblClick);
     this.canvas.addEventListener('mousedown', this.onMouseDown);
     this.canvas.addEventListener('mouseup', this.onMouseUp);
     this.canvas.addEventListener('mousemove', this.onMouseMove);
     this.canvas.addEventListener('mouseout', this.onMouseUp);
-    this.canvas.addEventListener('dblclick', this.onDblClick);
   }
 
   componentWillUnmount() {
+    this.canvas.removeEventListener('click', this.onClick);
+    this.canvas.removeEventListener('dblclick', this.onDblClick);
     this.canvas.removeEventListener('mousedown', this.onMouseDown);
     this.canvas.removeEventListener('mouseup', this.onMouseUp);
     this.canvas.removeEventListener('mousemove', this.onMouseMove);
     this.canvas.removeEventListener('mouseout', this.onMouseUp);
-    this.canvas.removeEventListener('dblclick', this.onDblClick);
   }
 
   onMouseDown = (e: MouseEvent): void => {
@@ -89,6 +96,31 @@ class Editor extends React.Component<EditorProps, EditorState> {
     this.renderCanvas();
   }
 
+  onClick = (e: MouseEvent): void => {
+    if (e.shiftKey) {
+      const pos = this.getMousePosOnCanvas(e);
+      if (this.state.arrowStart === null) {
+        this.state.circles.map((c: Circle, i: number) => {
+          if (c.isOnCircle(pos)) this.setState({ arrowStart: i });
+        });
+      } else {
+        this.state.circles.map((c: Circle, i: number) => {
+          if (c.isOnCircle(pos)) {
+            const start = this.state.circles[this.state.arrowStart].center;
+            const end = c.center;
+            const arrow = new Arrow(start, end);
+            const arrows = this.state.arrows.concat(arrow);
+            this.setState({
+              arrows: arrows,
+              arrowStart: null,
+            });
+            this.renderCanvas();
+          }
+        })
+      }
+    }
+  }
+
   getMousePosOnCanvas = (e: MouseEvent): Point => {
     const rect = this.canvas.getBoundingClientRect();
     return {
@@ -102,6 +134,7 @@ class Editor extends React.Component<EditorProps, EditorState> {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = '#2e3440';
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.state.arrows.map((a: Arrow) => a.draw(this.ctx));
     this.state.circles.map((c: Circle) => c.draw(this.ctx));
     this.ctx.restore();
   }

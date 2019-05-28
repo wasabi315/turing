5c87cfaee580e2bf2dd11c09979807e057bc7534
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-05-28 10:09:44 +0900

    Use Point

--- src/shapes/Arrow.ts
+++ src/shapes/Arrow.ts
@@ -1,22 +1,20 @@
+import { Point } from '../utils';
+
 class Arrow {
 
-  fromX: number;
-  fromY: number;
-  toX: number;
-  toY: number;
+  start: Point;
+  end: Point;
 
-  constructor(fx: number, fy: number, tx: number, ty: number) {
-    this.fromX = fx;
-    this.fromY = fy;
-    this.toX = tx;
-    this.toY = ty;
+  constructor(start: Point, end: Point) {
+    this.start = start;
+    this.end = end;
   }
 
   draw = (ctx: CanvasRenderingContext2D): void => {
     ctx.save();
     ctx.beginPath();
-    ctx.moveTo(this.fromX, this.fromY);
-    ctx.lineTo(this.toX, this.toY);
+    ctx.moveTo(this.start.x, this.start.y);
+    ctx.lineTo(this.end.x, this.end.y);
     ctx.lineCap = 'round';
     ctx.lineWidth = 5;
     ctx.strokeStyle = '#4c566a';
@@ -24,10 +22,10 @@ class Arrow {
     ctx.shadowColor = '#2e3440';
     ctx.stroke();
     ctx.beginPath();
-    const theta: number = Math.atan2(this.toY - this.fromY, this.toX - this.fromX);
-    ctx.moveTo(this.toX + 4 * Math.cos(theta), this.toY + 4 * Math.sin(theta));
-    ctx.lineTo(this.toX - 25 * Math.cos(theta + Math.PI / 6), this.toY - 25 * Math.sin(theta + Math.PI / 6));
-    ctx.lineTo(this.toX - 25 * Math.cos(theta - Math.PI / 6), this.toY - 25 * Math.sin(theta - Math.PI / 6));
+    const theta: number = Math.atan2(this.end.y - this.start.y, this.end.x - this.start.x);
+    ctx.moveTo(this.end.x + 4 * Math.cos(theta), this.end.y + 4 * Math.sin(theta));
+    ctx.lineTo(this.end.x - 25 * Math.cos(theta + Math.PI / 6), this.end.y - 25 * Math.sin(theta + Math.PI / 6));
+    ctx.lineTo(this.end.x - 25 * Math.cos(theta - Math.PI / 6), this.end.y - 25 * Math.sin(theta - Math.PI / 6));
     ctx.closePath();
     ctx.shadowBlur = 0;
     ctx.shadowColor = 'rgba(0, 0, 0, 0)'

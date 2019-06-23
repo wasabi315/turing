891e5eb2311c1bce010e59e41548a52716821f91
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-06-23 21:14:43 +0900

    Use konva classes instead of canvas

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import Circle from '../shapes/Circle';
-import Arrow from '../shapes/Arrow';
-import Canvas from './Canvas';
+import { KonvaEventObject } from 'konva/types/Node';
+import { Layer, Rect, Stage } from 'react-konva';
+import Graph from '../../lib/Graph';
 import { Point } from '../../lib/Point';
+import Node from './Node';
+import Edge from './Edge';
+import Loop from './Loop';
 
 interface EditorProps {
   width: number;
@@ -10,10 +13,8 @@ interface EditorProps {
 }
 
 interface EditorState {
-  arrows: Arrow[];
-  circles: Circle[];
-  arrowStart: number | null;
-  dragTarget: number | null;
+  graph: Graph;
+  nodePos: Point[];
 }
 
 class Editor extends React.Component<EditorProps, EditorState> {
@@ -21,99 +22,26 @@ class Editor extends React.Component<EditorProps, EditorState> {
   constructor(props: EditorProps) {
     super(props);
     this.state = {
-      arrows: [],
-      circles: [],
-      arrowStart: null,
-      dragTarget: null,
+      graph: new Graph(),
+      nodePos: [],
     };
-  }
-
-  onClickNode = (i: number) => {
-    if(this.state.arrowStart !== null) {
-      const start = this.state.circles[this.state.arrowStart].center;
-      const end = this.state.circles[i].center;
-      const arrow = new Arrow(start, end);
-      arrow.shrink(55);
-      const arrows = this.state.arrows.concat(arrow);
-      this.setState({
-        arrows: arrows,
-        arrowStart: null,
-      });
-    } else {
-      this.setState({ arrowStart: i });
-    }
-  }
-
-  addNode = (p: Point) => {
-    const circle = new Circle(p, 40, this.state.circles.length);
-    const circles = this.state.circles.concat(circle);
-    this.setState({ circles: circles });
-  }
-
-  withClickPos = (p: Point) => {
-    let b = true;
-    this.state.circles.map((c: Circle, i: number) => {
-      if(c.isOnCircle(p)) {
-        this.onClickNode(i);
-        b = false;
-      }
-    });
-    if(b) this.addNode(p);
-  }
-
-  withDblClickPos = (_: Point) => alert('dblclick');
-
-  withMouseDownPos = (p: Point) => {
-    this.state.circles.map((c: Circle, i: number) => {
-      if(c.isOnCircle(p)) {
-        this.setState({ dragTarget: i });
-        return;
-      }
-    });
-  }
-
-  withMouseMovePos = (p: Point) => {
-    const circles = this.state.circles.slice();
-    circles.map((c: Circle, i: number) => {
-      if(i === this.state.dragTarget) { c.center = p };
-    });
-    this.setState({ circles: circles });
-  }
-
-  withMouseUpPos = (_: Point) => {
-    this.setState({ dragTarget: null });
-  }
-
-  onMouseOut = () => {
-    this.setState({
-      dragTarget: null,
-    });
-  }
-
-  draw = (ctx: CanvasRenderingContext2D) => {
-    ctx.save();
-    ctx.clearRect(0, 0, this.props.width, this.props.height);
-    ctx.fillStyle = '#2e3440';
-    ctx.fillRect(0, 0, this.props.width, this.props.height);
-    this.state.arrows.map((a: Arrow) => a.draw(ctx));
-    this.state.circles.map((c: Circle) => c.draw(ctx));
-    ctx.restore();
   }
 
   render() {
     return (
-      <Canvas
+      <Stage
         width={this.props.width}
         height={this.props.height}
-        draw={this.draw}
-        withClickPos={this.withClickPos}
-        withDblClickPos={this.withDblClickPos}
-        withMouseDownPos={this.withMouseDownPos}
-        withMouseMovePos={this.withMouseMovePos}
-        withMouseUpPos={this.withMouseUpPos}
-        onMouseOut={this.onMouseOut}
-      />
-    )
+      >
+        <Layer>
+          <Rect
+            width={this.props.width}
+            height={this.props.height}
+            fill="#2e3440"
+          />
+        </Layer>
+      </Stage>
+    );
   }
 
 }

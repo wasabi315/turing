1c8e5c96b485e936af1523dafb1168e14441f665
Satoshi Takimoto <wasabi.nvim.st@gmail.com>
2019-06-25 16:45:00 +0900

    Fix styles

--- src/components/Edge.tsx
+++ src/components/Edge.tsx
@@ -11,15 +11,24 @@ interface EdgeProps {
 
 const Edge: React.SFC<EdgeProps> = props => {
 
+  const { x: startX, y: startY } = props.startPos;
+  const { x: endX, y: endY } = props.endPos;
+  const theta = Math.atan2(endY - startY, endX - startX);
   const points =
-    [ props.startPos.x, props.startPos.y,
-      props.endPos.x, props.endPos.y
+    [ props.startPos.x + 45 * Math.cos(theta),
+      props.startPos.y + 45 * Math.sin(theta),
+      props.endPos.x - 45 * Math.cos(theta),
+      props.endPos.y - 45 * Math.sin(theta)
     ];
   return (
     <Arrow
       points={points}
       fill="#4c566a"
       stroke="#4c566a"
+      shadowColor="#2e3440"
+      shadowBlur={5}
+      shadowOffsetX={5}
+      shadowOffsetY={5}
       strokeWidth={4}
       pointerWidth={20}
       pointerLength={20}
